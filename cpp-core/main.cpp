49428a69523a59a7432e3b0dd3972bdb7a92a79a
“dbzqxsy” <njhkht_xys@outlook.com>
2025-09-23 22:24:01 +0800

    重构主程序，添加支出记录输入功能，支持日期、金额和类型的动态输入

--- cpp-core/main.cpp
+++ cpp-core/main.cpp
@@ -1,11 +1,33 @@
 #include<iostream>
-#include"expense.hpp"
+#include<vector>
+#include "expense.hpp"
 
 int main(){
-    Expense a(11.0,"午饭","2025-9-23");
-    std::cout << "原始数据" << std::endl;
-    std::cout << "金额" << a.getAmount() << std::endl;
-    std::cout << "类型" << a.getCategory() << std::endl;
-    std::cout << "日期" << a.getData() << std::endl;
+    std::string data;
+    std::cout << "请输入日期（格式为YYYY-MM-DD）" ;
+    std::cin >> data;
+
+    std::vector<Expense> records;
+    while(true){
+        double amount;
+        std::string category;
+        std::cout << "请输入金额（输入-1结束）：";
+        std::cin >> amount;
+        if(amount == -1){
+            break;
+        }
+        std::cout << "请输入类型：";
+        std::cin >> category;
+
+        Expense e(amount,category,data);
+        records.push_back(e);
+    }
+    std::cout << "输入完成，支出如下：" << std::endl;
+    for(const auto& e : records){
+        std::cout << "金额：" << e.getAmount()
+                <<"类型：" << e.getCategory()
+                << std::endl;
+    }
+    std::cout << "日期：" << data << std::endl;
     return 0;
 }
